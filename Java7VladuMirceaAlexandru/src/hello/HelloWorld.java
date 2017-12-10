5f511adb324e9390afc5e3ae79d7c67fc032f2c3
vladu <vladu@Alex-Lenovo.agro.local>
2017-12-10 16:05:25 +0200

    Ce invatam la CURS

--- Java7VladuMirceaAlexandru/src/hello/HelloWorld.java
+++ Java7VladuMirceaAlexandru/src/hello/HelloWorld.java
@@ -15,5 +15,4 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello World");
-}
+     System.out.println("Hello World");}}
